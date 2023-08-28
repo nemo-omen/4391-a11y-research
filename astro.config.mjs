a135b6e0e98a3851993944a62aea432180710656
nemo-omen <nemo.omen@gmail.com>
2023-08-27 19:22:11 -0500

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://nemo-omen.github.io',
-  base: '4391-a11y-research'
-});
+export default defineConfig({});
