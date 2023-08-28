2e86cafba47f3873bf761eaa7cda97d4ac866ec3
nemo-omen <nemo.omen@gmail.com>
2023-08-28 08:54:55 -0500

    First pass at centering citation card in margin

--- src/components/CitationCard.svelte
+++ src/components/CitationCard.svelte
@@ -1,11 +1,33 @@
 <script>
+  import { onMount } from 'svelte';
   import { fly } from 'svelte/transition';
+
+  let card;
+  $: offset = 1;
 
   export let title;
   export let content;
+
+  onMount(() => {
+    const mainSelection = document.querySelector('main');
+    const articleSelection = document.querySelector('#main-content');
+    const articleWidth = articleSelection.scrollWidth;
+    const articleHorizontal = articleSelection.scrollLeft + articleWidth;
+    const mainWidth = mainSelection.clientWidth;
+    const available = mainWidth - articleHorizontal;
+    const doubleOffset = available - card.clientWidth;
+    offset = doubleOffset / 2;
+    console.log(offset);
+  });
 </script>
 
-<div class="hover-card" role="tooltip" transition:fly={{ duration: 700, x: 500 }}>
+<div
+  class="hover-card"
+  bind:this={card}
+  role="tooltip"
+  style:--offset="{offset}px"
+  transition:fly={{ duration: 700, x: 500 }}
+>
   <h3 class="card-title">{title}</h3>
   {@html content}
 </div>
@@ -22,10 +44,10 @@
     box-shadow: 0px 0px 30px 5px rgb(0, 0, 0, 0.025);
     border-radius: 0.5em;
     padding: 1em;
-    width: 30em;
+    width: 20em;
     height: auto;
     top: 50%;
-    right: 2.5em;
+    right: var(--offset);
     transform: translateY(-50%);
     border: 1px solid var(--color-primary);
     font-family: var(--font-mono);
