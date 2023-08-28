1dac6059c737c653ff323f409bcc6f08b6d71d4b
nemo-omen <nemo.omen@gmail.com>
2023-08-27 19:02:20 -0500

    Init gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://nemo-omen.github.io',
+  base: '4391-a11y-research'
+});
